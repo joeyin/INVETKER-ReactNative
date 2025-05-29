b04d562eb6122046b779ecb54593e9f13a8a36b5
Chi-Wei <joeyin@good-series.com>
2025-05-29 17:47:50 -0400

    Improve authorization

--- providers/AppProvider.tsx
+++ providers/AppProvider.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { UserCredential, User } from "firebase/auth";
+import { User, onAuthStateChanged, getAuth } from "firebase/auth";
 import accountController from "../controllers/accountController";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface AppContextType {
   user: User | null;
-  signIn: (email: string, password: string, rememberMe: boolean) => Promise<User>;
+  signIn: (
+    email: string,
+    password: string,
+    rememberMe: boolean
+  ) => Promise<User>;
   signOut: () => void;
 }
 
@@ -24,26 +28,39 @@ export const useApp = () => {
 export const AppProvider = ({ children }) => {
   const [user, setUser] = React.useState<User | null>(null);
 
+  React.useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const handleAuthStateChanged = React.useCallback((user) => {
+    setUser(user);
+  }, []);
+
   const signIn = React.useCallback(
     async (email: string, password: string, rememberMe: boolean = false) => {
       try {
         const userCredential = await accountController.signIn(email, password);
-        setUser(userCredential.user);
         if (rememberMe) {
           await AsyncStorage.setItem("email", email);
           await AsyncStorage.setItem("password", password);
         }
         return userCredential.user;
       } catch (error) {
-        console.error("Login failed:", error);
+        console.error("Sign in failed:", error);
         throw error;
       }
     },
     []
   );
 
-  const signOut = React.useCallback(() => {
-    setUser(null);
+  const signOut = React.useCallback(async () => {
+    try {
+      await accountController.signOut();
+    } catch (error) {
+      console.error("Sign out failed:", error);
+      throw error;
+    }
   }, []);
 
   return (
