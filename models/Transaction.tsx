c32742487567a3fe8d7d67c9eb832247b613511a
Chi-Wei <joeyin@good-series.com>
2025-06-04 17:19:01 -0400

    Add models/Ticker and improvement

--- models/Transaction.tsx
+++ models/Transaction.tsx
@@ -1,28 +1,22 @@
 export enum Action {
-  BUY = 'BUY',
-  SELL = 'SELL',
-}
-
-export enum Symbol {
-  TSLA = 'TSLA',
-  AAPL = 'AAPL',
-  MSFT = 'MSFT',
-  HSBC = 'HSBC',
+  BUY = "BUY",
+  SELL = "SELL",
 }
 
 export interface Transaction {
-  id: string
-  userId: string
-  ticker: string
-  quantity: number
-  action: Action
-  price: number
-  fee: number
-  date: Date
+  id: string;
+  userId: string;
+  ticker: string;
+  quantity: number;
+  action: Action;
+  price: number;
+  fee: number;
+  date: Date;
 }
 
-export interface NewTransaction extends Omit<Transaction, 'id' | 'userId' | 'date'> {
+export interface NewTransaction
+  extends Omit<Transaction, "id" | "userId" | "date"> {
   id?: string;
   userId?: string;
-  date: number
+  date: number;
 }
